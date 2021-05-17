71cc1916826ef77282dcd6a82dd00edf9b224eac
Francisco Carvalho <fiskolini@gmail.com>
2021-05-17 02:18:13 +0100

    Update .env.example with correct DB host + uncomment Middleware

--- app/src/Infrastructure/Http/WebKernel.php
+++ app/src/Infrastructure/Http/WebKernel.php
@@ -55,7 +55,7 @@ class WebKernel
     private function middlewares(): array
     {
         return [
-            //Middleware\DummyAuthMiddleware::class,
+            Middleware\DummyAuthMiddleware::class,
             Middleware\ErrorHandlerMiddleware::class,
         ];
     }
